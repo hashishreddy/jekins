1472c38ab94d9353432c8ced69447bebd48c3813
hashishreddy <146133960+hashishreddy@users.noreply.github.com>
2025-12-09 11:24:54 +0530

    Update launch.java

--- launch.java
+++ launch.java
@@ -1,6 +1,6 @@
 public class launch {
     public static void main(String[] args) {
-        System.out.println("Java program launched successfully from Jenkins Workspace!");
+        System.out.println("Java program launched successfully from Jenkins Workspace! modda gudu");
         int a = 15;
         int b = 25;
         System.out.println("Sum = " + (a + b));
